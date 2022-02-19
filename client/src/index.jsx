5778c023ed3a6dc3f602a52b2f0344e4661d81f1
timothyAgevi <timothyagevi@gmail.com>
2022-02-19 16:09:57 +0300

    edit index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import ReactDom from"react-dom"
+import App from "./App"
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
-}
 
-export default index
+ReactDom.render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>
+    ,document.getElementById("root")
+)
+
