b3646dc0ac7b848da95a8f11619df94f49221230
timothyAgevi <timothyagevi@gmail.com>
2022-02-20 16:48:47 +0300

    add dburl to config

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -5,9 +5,10 @@ dotenv.config()
 const PORT = process.env.PORT
 
 const SECRTET_KEY=process.env.SECRTET_KEY
+
 const DATABASE_URL=process.env.DATABASE_URL
 
-module.exports = Object.freeze({ PORT, SECRTET_KEY})
+module.exports = Object.freeze({ PORT, SECRTET_KEY,DATABASE_URL})
 /**
  * 
  */
