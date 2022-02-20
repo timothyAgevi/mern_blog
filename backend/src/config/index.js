15ebb67bd4bbfade815227ca0409706e47ad76da
timothyAgevi <timothyagevi@gmail.com>
2022-02-20 17:24:39 +0300

    update

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -4,11 +4,11 @@ dotenv.config()
 
 const PORT = process.env.PORT
 
-const SECRTET_KEY=process.env.SECRTET_KEY
+const SECRET_KEY=process.env.SECRET_KEY
 
 const DATABASE_URL=process.env.DATABASE_URL
 
-module.exports = Object.freeze({ PORT, SECRTET_KEY,DATABASE_URL})
+module.exports = Object.freeze({ PORT, SECRET_KEY,DATABASE_URL})
 /**
  * 
  */
