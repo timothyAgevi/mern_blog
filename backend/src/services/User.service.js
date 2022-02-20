741eee1d5d2d0ccb1a48901c8d4475a3fa3ce18a
timothyAgevi <timothyagevi@gmail.com>
2022-02-21 00:42:05 +0300

    add user service

--- backend/src/services/User.service.js
+++ backend/src/services/User.service.js
@@ -1,8 +1,39 @@
-const{ Request,Response,NextFunction}=require("express")
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-unused-vars
+const { Request, Response, NextFunction } = require("express")
 
-class UseController{
-     register = async(req,res,next)=>{
-         return res.json()
+class UseController {
+	/**
+	 *
+	 * @param {Reaquest} req
+	 * @param {Response} res
+	 * @param {NextFunction} next
+	 * @returns
+	 */
+	register = async (req, res, next) => {
+		return res.json({ message: "Register" })
+	}
+	/**
+	 *
+	 * @param {Reaquest} req
+	 * @param {Response} res
+	 * @param {NextFunction} next
+	 * @returns
+	 */
+	login = async (req, res, next) => {
+		return res.json({ message: "Login" })
+	}
 
-     }
+	/**
+	 *
+	 * @param {Reaquest} req
+	 * @param {Response} res
+	 * @param {NextFunction} next
+	 * @returns
+	 */
+	updateprofile = async (req, res, next) => {
+		return res.json({ message: "Update profile" })
+	}
 }
+
+module.exports = UseController
