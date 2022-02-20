f8925c9e3e729c74817d85cec602c94508bdd4ce
timothyAgevi <timothyagevi@gmail.com>
2022-02-20 23:40:58 +0300

    update settings

--- backend/src/settings/index.js
+++ backend/src/settings/index.js
@@ -1,22 +1,22 @@
-const {Application} = require("express")
+// eslint-disable-next-line no-unused-vars
+const { Application } = require("express")
 const express = require("express")
 const cors = require("cors")
 const logger = require("morgan")
 const helmet = require("helmet")
-const db = require("../../db")
-const {DATABASE_URL}=require(" ./../config")
-const { routes } = require("../app")
-console.log(DATABASE_URL)
+const db = require("./../db")
+const { DATABASE_URL } = require("./../config")
+const routes = require("../routes")
 /**
- * 
- * @param {{app:Application}} param0 
+ *
+ * @param {{app:Application}} param0
  */
-module.exports =({ app })=>{
- app.use(express.json ({limit:"100mb"}))
- app.use(express.urlencoded({extended:true}))
- app.use(logger("combined"))
- app.use(helmet ())
- app.use(cors({ origin :"*"}))
- db({ db_url:DATABASE_URL })
-routes({ app })
+module.exports = ({ app }) => {
+	app.use(express.json({ limit: "100mb" }))
+	app.use(express.urlencoded({ extended: true }))
+	app.use(logger("combined"))
+	app.use(helmet())
+	db({ db_url: DATABASE_URL })
+	app.use(cors({ origin: "*" }))
+	routes({ app })
 }
