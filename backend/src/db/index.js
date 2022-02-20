8bf09b9a842d9150da0d5df6c43440e576073a9d
timothyAgevi <timothyagevi@gmail.com>
2022-02-20 22:04:20 +0300

    add db

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,7 +3,7 @@ const { connect }= require("mongoose")
 /**
  * @param {{db_url:string}} param0
  */
-module.exports = function ( { db_url}) {
+module.exports = function ( { db_url} ) {
     connect(db_url).then( () => {
         console.log("Database connection sucessful")
     }).catch( err=>{
