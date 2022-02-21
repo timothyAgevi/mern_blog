54b37f159b77ab0f2d0f9ff6618ba57898ec2618
timothyAgevi <timothyagevi@gmail.com>
2022-02-21 05:50:19 +0300

    add Nav component

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from " ./Nav.module.css";
 
 const Nav = () => {
   return (
     <div>
-<nav>
-        <Link to ={"/"}> Homepage</Link> 
-        <Link to ={"/login"}>Login</Link> 
-        <Link to ={"/register"}>Register</Link> 
-</nav>
-
+      <nav className= "nav">
+        <Link to={"/"}> Homepage</Link>
+        <Link to={"/login"}>Login</Link>
+        <Link to={"/register"}>Register</Link>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
