e51e1b4b97059dfb9bd3d260ea7a3f88aed19748
timothyAgevi <timothyagevi@gmail.com>
2022-02-21 05:11:29 +0300

    update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,11 @@ import { HashRouter as Router,Link,Route,Routes}from 'react-router-dom'
 const App = () => {
   return (
     <Router>
-      <nav> <Link to ={"/"}> Homepage</Link> 
+      <nav> 
+        { " "}
+        <Link to ={"/"}> Homepage</Link> 
+        <Link to ={"/login"}>Login</Link> 
+        <Link to ={"/register"}>Register</Link> 
       </nav>
       <Routes>
       <Route path ="/" element={< Homepage />}/>
